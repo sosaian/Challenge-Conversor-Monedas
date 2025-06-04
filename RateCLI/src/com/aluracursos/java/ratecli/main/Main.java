afae0cd34168bbc335c91c791369e3ae60f4bf59
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-06-04 14:30:18 -0300

    Add initial API request for all supported currencies

--- RateCLI/src/com/aluracursos/java/ratecli/main/Main.java
+++ RateCLI/src/com/aluracursos/java/ratecli/main/Main.java
@@ -1,7 +1,28 @@
 package com.aluracursos.java.ratecli.main;
 
+import com.aluracursos.java.ratecli.http.RequestHandler;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bienvenido a RateCLI!");
+        System.out.println("Preparando todo para iniciar el programa...");
+
+        // Carga de API KEY desde variables de entorno del SO
+        String codesListURL =
+                "https://v6.exchangerate-api.com/v6/"
+                + System.getenv("RATECLI_API_KEY")
+                + "/codes";
+
+        // Solicitud de todas las divisas disponibles para operar
+        String codesListResponse;
+
+        try {
+            codesListResponse = RequestHandler.makeRequest(codesListURL);
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(codesListResponse);
     }
 }
