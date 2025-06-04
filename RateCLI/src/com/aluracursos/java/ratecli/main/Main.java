114c2ac11298fb226bc7e336fe32579815af4cb5
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-06-04 17:09:15 -0300

    Add UX to enable multiple exchange rates requests

--- RateCLI/src/com/aluracursos/java/ratecli/main/Main.java
+++ RateCLI/src/com/aluracursos/java/ratecli/main/Main.java
@@ -6,6 +6,8 @@ import com.aluracursos.java.ratecli.models.Currency;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -34,10 +36,72 @@ public class Main {
             return;
         }
 
+        // Inicio del UX
+        System.out.println("🛍️ Bienvenido a RateCLI!");
+        System.out.println("Por favor, ingresa el código de divisa desde la que quieras hacer conversiones.");
+        System.out.println("\n" + "*".repeat(30) + "\n");
+
+        Scanner scanner = new Scanner(System.in);
+        String userResponse;
+        boolean invalidUserResponse;
+        boolean firstLoop = true;
+        boolean restartLoop;
+
         // Listar las divisas disponibles al usuario
         System.out.println("Listado de divisas disponibles:\n");
         codesListParsedResponse.forEach(
                 (key, value) -> System.out.println(key + " - " + value.name())
         );
+
+        do {
+            userResponse = "0";
+            invalidUserResponse = true;
+            restartLoop = false;
+
+            if (!firstLoop) System.out.println("\nEscribe 1 para listar todas las divisas disponibles.");
+            System.out.println("\nEscribe 0 para salir.");
+            System.out.println("\n" + "*".repeat(30));
+
+            // Solicitar la divisa a referenciar para las conversiones
+            while (invalidUserResponse) {
+                try {
+                    System.out.print("\n💵 Divisa a comparar: ");
+                    userResponse = scanner.nextLine().trim().toUpperCase();
+
+                    if (userResponse.equals("0")) {
+                        System.out.println("👋🏿 Gracias por usar RateCLI, cerrando programa...");
+                        return;
+                    } else if (!firstLoop && userResponse.equals("1")) {
+                        System.out.println("\nListado de divisas disponibles:\n");
+                        codesListParsedResponse.forEach(
+                                (key, value) -> System.out.println(key + " - " + value.name())
+                        );
+                        System.out.println("\nEscribe 1 para listar todas las divisas disponibles.");
+                        System.out.println("\nEscribe 0 para salir.");
+                        System.out.println("\n" + "*".repeat(30));
+                        restartLoop = true;
+                    } else if (codesListParsedResponse.get(userResponse) == null) {
+                        System.out.println("⚠️ El valor ingresado no es válido. Debe ser un código del listado.");
+                    } else {
+                        invalidUserResponse = false; // Cambiar el estado a válido si el código es válido.
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("❌ Código inválido. " +
+                            "Por favor, ingresa un código de divisa (por ejemplo, ARS).");
+                }
+            }
+
+            if (restartLoop) continue;
+
+            System.out.println("Genial! has elegido la divisa: "
+                    + codesListParsedResponse.get(userResponse.toUpperCase()));
+            firstLoop = false;
+
+            System.out.println("\n" + "*".repeat(30) + "\n");
+            System.out.print("Presione la tecla Enter para continuar...");
+            scanner.nextLine();
+            System.out.println("\n" + "*".repeat(30) + "\n");
+            System.out.println("Por favor, ingresa el código de divisa desde la que quieras hacer conversiones.");
+        } while(true);
     }
 }
