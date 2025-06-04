94f7e409c59a449a78ae9f6f5a83183cdf74ff5e
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-06-04 15:24:01 -0300

    Add codeListResponse parsing capabilities into Main.java

--- RateCLI/src/com/aluracursos/java/ratecli/main/Main.java
+++ RateCLI/src/com/aluracursos/java/ratecli/main/Main.java
@@ -1,15 +1,18 @@
 package com.aluracursos.java.ratecli.main;
 
 import com.aluracursos.java.ratecli.http.RequestHandler;
+import com.aluracursos.java.ratecli.io.CurrencyCodesDeserializer;
+import com.aluracursos.java.ratecli.models.Currency;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Preparando todo para iniciar el programa...");
 
         // Carga de API KEY desde variables de entorno del SO
-        String codesListURL =
+        String currencyCodesListURL =
                 "https://v6.exchangerate-api.com/v6/"
                 + System.getenv("RATECLI_API_KEY")
                 + "/codes";
@@ -18,11 +21,20 @@ public class Main {
         String codesListResponse;
 
         try {
-            codesListResponse = RequestHandler.makeRequest(codesListURL);
+            codesListResponse = RequestHandler.makeRequest(currencyCodesListURL);
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
 
-        System.out.println(codesListResponse);
+        // Conversión de la respuesta recibida a una lista que se pueda manipular en este código
+        List<Currency> codesListParsedResponse = CurrencyCodesDeserializer.parseCurrencyCodeListResponse(codesListResponse);
+
+        if (codesListParsedResponse.isEmpty()) {
+            System.out.println("️⚠️ Ha ocurrido un error cargando la información. Intente de nuevo más tarde.");
+            return;
+        }
+
+        System.out.println("Listado de divisas disponibles:\n");
+        codesListParsedResponse.forEach(System.out::println);
     }
 }
