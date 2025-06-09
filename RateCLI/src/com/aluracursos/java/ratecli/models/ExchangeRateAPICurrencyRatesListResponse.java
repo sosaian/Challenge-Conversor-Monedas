47c793f29850e52e503862381dcd81471394d6d4
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-06-09 15:45:04 -0300

    Add time_last_update_utc to ExchangeRateAPICurrencyRatesListResponse

--- RateCLI/src/com/aluracursos/java/ratecli/models/ExchangeRateAPICurrencyRatesListResponse.java
+++ RateCLI/src/com/aluracursos/java/ratecli/models/ExchangeRateAPICurrencyRatesListResponse.java
@@ -2,16 +2,17 @@ package com.aluracursos.java.ratecli.models;
 
 import java.util.Map;
 
-public record ExchangeRateAPICurrencyRatesListResponse(Map<String, Double> conversion_rates) {
+public record ExchangeRateAPICurrencyRatesListResponse(
+        Map<String,Double> conversion_rates,
+        String time_last_update_utc) {
 /*
     Listado de atributos recibidos por ExchangeRate API a la fecha de este commit.
-    De momento solo necesito conversion_rates, dejando los demás comentados.
+    De momento solo necesito conversion_rates y time_next_update_utc, dejando los demás comentados.
 
     private String result;
     private String documentation;
     private String terms_of_use;
     private long time_last_update_unix;
-    private String time_last_update_utc;
     private long time_next_update_unix;
     private String time_next_update_utc;
     private String base_code;
